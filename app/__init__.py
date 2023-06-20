efbcaaf39e37332c879bd96718d1bbc8ca66dbb1
Andrew Subotin <subotinandrey5@gmail.com>
2023-06-20 11:24:59 +0300

    Migrated to the first migration

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 db = SQLAlchemy()
 migrate = Migrate()
+
+# with app.app_context():
+#     from . import routes
+#     db.create_all()
 
 
 def create_app(test_config=None):
@@ -16,10 +21,11 @@ def create_app(test_config=None):
     else:
         app.config.from_object('config.ProductionConfig')
 
+    # This import is necessary so that models
+    # are detected by Flask-Migrate
+    from . import models
+
     db.init_app(app)
     migrate.init_app(app, db)
-
-    with app.app_context():
-        db.create_all()
 
     return app
