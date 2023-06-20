5dd0a0b1c1e9b2e24c040a8351b525ad6c0e8777
Andrew Subotin <subotinandrey5@gmail.com>
2023-06-20 13:18:50 +0300

    Signup route

--- app/auth.py
+++ app/auth.py
@@ -1,16 +1,42 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/signup/', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+
+        user_with_email = User.query.filter_by(email=email).first()
+        user_with_username = User.query.filter_by(username=username).first()
+
+        if user_with_email:
+            flash('Email already exists.')
+            return redirect(url_for('auth.signup'))
+        if user_with_username:
+            flash('Username already exists.')
+            return redirect(url_for('auth.signup'))
+
+        new_user = User(email=email, username=username,
+                        password=generate_password_hash(password))
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/signup.html')
 
 
 @bp.route('/login/')
 def login():
     return render_template('auth/login.html')
-
-
-@bp.route('/signup/')
-def signup():
-    return render_template('auth/signup.html')
 
 
 @bp.route('/logout/')
--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     def __str__(self):
         return self.username
